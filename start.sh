06286d43887b830bab2f2ae5351f8731d905657c
Aleksandr Didenko <adidenko@mirantis.com>
2014-04-11 19:07:59 +0300

    Added syncdb and load fixtures into start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 
 puppet apply -d -v /root/site.pp
 
+/opt/nailgun/bin/nailgun_syncdb
+/opt/nailgun/bin/nailgun_fixtures
+
 /etc/init.d/supervisord stop
+
 ln -s /etc/fuel/nailgun/version.yaml /etc/nailgun/version.yaml
 
 /usr/bin/supervisord -n
