bcb82bc762134218a7f1aeea0e9f9efc838a2fe2
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-14 17:37:09 +0400

    fix db commands

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ln -s /etc/fuel/nailgun/version.yaml /etc/nailgun/version.yaml || true
-puppet apply -v /etc/puppet/modules/nailgun/examples/nailgun-only.pp
+puppet apply -v -d /etc/puppet/modules/nailgun/examples/nailgun-only.pp
 
-/opt/nailgun/bin/nailgun_syncdb
-/opt/nailgun/bin/nailgun_fixtures
+/usr/bin//nailgun_syncdb
+/usr/bin/nailgun_fixtures
 
 pgrep supervisord && /etc/init.d/supervisord stop
-
 
 /usr/bin/supervisord -n
 
